29b57a5c67619e076d1d6f5a99be86b7083e56fb
VeroPolegre <veronicapolegre98@gmail.com>
2023-09-25 14:10:15 +0200

    add/more-exercises

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Condicionales
 Bucles
 
 Imprime todos los números del 1 al 1000 por consola, con la salvedad de todos aquellos números que sean múltiplos de 3 que en vez de imprimir el número, se imprimirá Fizz, en los números que sean múltiplos de 5 se imprimirá Buzz, y además en los números que sean múltiplos de 3 y de 5 se imprimirá FizzBuzz.
+
+# orden
+
+Dada una lista ordenada de números enteros, eleva al cuadrado los elementos y proporciona el resultado en orden.
+Por ejemplo, dado [-9, -2, 0, 2, 3], devuelve [0, 4, 4, 9, 81].
--- script.js
+++ script.js
@@ -1,13 +1,72 @@
+/* 
+//fizzbuzz
 let num = 0;
 
-for (let num = 1; num <= 1000; num++) {
-  if (num % 3 === 0) {
-    console.log("Fizz");
-  } else if (num % 5 === 0) {
+ for (let num = 1; num <= 1000; num++) {
+   if (num % 3 === 0) {
+     console.log("Fizz");
+   } else if (num % 5 === 0) {
     console.log("Buzz");
-  } else if (num % 3 === 0 && num % 5 === 0) {
-    console.log("FizzBuzz");
-  } else {
-    console.log(num);
+   } else if (num % 3 === 0 && num % 5 === 0) {
+     console.log("FizzBuzz");
+   } else {
+     console.log(num);
   }
+ }
+/*
+//elevar al cuadrado
+ const listaDesordenada = [-9, -2, 0, 2, 3];
+
+ function elevarAlCuadrado(lista) {
+   const listaOrdenada = [];
+   for (let i = 0; i < lista.length; i++) {
+     listaOrdenada.push(lista[i] ** 2);
+     listaOrdenada.sort();
+   }
+
+   return listaOrdenada;
+ }
+
+ const listaOrdenada = elevarAlCuadrado(listaDesordenada);
+ console.log(listaOrdenada); */
+
+/*
+//tabla multiplicar
+
+function tablaDeMultiplicar(num) {
+  const tabla = [];
+  for (let i = 1; i <= 10; i++) {
+    const resultado = num * i;
+    tabla.push(`${num}x${i}=${resultado}`);
+  }
+  return tabla;
 }
+//prompt
+const num = parseInt(prompt("Introduce un numerito"));
+if (num) {
+  const tabla = tablaDeMultiplicar(num);
+  console.log(tabla);
+}
+
+*/
+
+function ingresarNum() {
+  const resultado = [];
+  while ((numero = parseInt)) {
+    const numero = parseInt(prompt("Ingrese números"));
+    if (numero === 0) {
+      break;
+    }
+    resultado.push(numero); //añadir numeros al array
+  }
+
+  resultado.sort(function (a, b) {
+    return a - b; //numeros ordenados
+  });
+
+  return resultado;
+}
+
+//resultado ordenado
+const listaOrdenados = ingresarNum();
+console.log(listaOrdenados);
